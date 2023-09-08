9c6409404e0eccec3f975925901dcf80e3540ff1
eharmancioglu <erdem.harmancioglu@enuygun.com>
2023-09-08 23:33:20 +0300

    code structure updated

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/harmancioglue/go-hexagonal-architecture/internal/core/controller"
-	"github.com/harmancioglue/go-hexagonal-architecture/internal/core/infra/repository"
+	"github.com/harmancioglue/go-hexagonal-architecture/internal/controller"
 	"github.com/harmancioglue/go-hexagonal-architecture/internal/core/service"
+	repository2 "github.com/harmancioglue/go-hexagonal-architecture/internal/infra/repository"
 	"github.com/joho/godotenv"
 )
 
@@ -12,13 +12,13 @@ func main() {
 	godotenv.Load(".env")
 
 	//create db
-	db, err := repository.NewDatabase()
+	db, err := repository2.NewDatabase()
 	if err != nil {
 
 	}
 
 	//create repository
-	todoRepository := repository.NewTodoRepository(db)
+	todoRepository := repository2.NewTodoRepository(db)
 	//create service
 	todoService := service.NewTodoService(todoRepository)
 	//create controller
